6879caee0c26d4dd856294844bb820fe13664828
VijayBharghav3011 <vijaykumar3012@icloud.com>
2025-01-11 13:09:26 +0530

    Delete frontend directory

--- frontend/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import to_do_list
-
-urlpatterns = [
-    path('', to_do_list, name='to_do'),
-]
--- frontend/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def to_do_list(request):
-    return render(request, 'todo.html')
